10c6fe2cc57791624107562e7b3c6795ca1869c3
Aiden <aidenshih@gmail.com>
2018-03-19 12:45:11 -0400

    Added Utilities folder for helper functions and repeat components

---
+++ frontend/src/components/Utilities/Reuseables.jsx
@@ -0,0 +1,8 @@
+const Reuseables = {
+    logoIcon: function getLogoIcon(props) {
+        return <img src={props.url} alt="RoomEase" />
+    },
+    logoName:  function getLogoName(props) {
+        return <img src={props.url} alt="RoomEase" />
+    }
+}
