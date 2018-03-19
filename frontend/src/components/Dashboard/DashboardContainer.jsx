794958826f8212f6e0746ef3932fcab910ded7c5
Aiden <aidenshih@gmail.com>
2018-03-19 11:19:12 -0400

    Added boilerplate and skeleton for Redux

---
+++ frontend/src/components/Dashboard/DashboardContainer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Dashboard from './Dashboard';
+
+class DashboardContainer extends Component {
+    render() {
+        return (
+            <div>
+                <Dashboard />
+            </div>
+        );
+    }
+}
+
+export default DashboardContainer;
---
+++ frontend/src/components/Homepage/Homepage.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Homepage = () => (
+  <div>
+    <h2>Homepage</h2>
+  </div>
+)
+
+export default Homepage
