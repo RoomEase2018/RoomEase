b972894dfd83e7d407c0b8acd77a4782b64d5af4
Ruben Profit <rubenprofit@ac.c4q.nyc>
2018-03-21 13:46:46 -0400

    added queries and routes for server

--- backend/routes/expenses.js
+++ backend/routes/expenses.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+let db = require("../db/queries");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* GET expenses. */
+router.get("/user/:userid", db.getExpensesByUserId);
+
+router.get("/apartment/:aptid", db.getExpensesByApartmentId);
+
+router.get("/fixed/:aptid", db.getFixedExpensesByApartmentId);
 
 module.exports = router;
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+let db = require("../db/queries");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/:aptid", db.getNotesByApartmentId);
 
 module.exports = router;
