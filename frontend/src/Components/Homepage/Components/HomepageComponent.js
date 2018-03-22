b9d6344445bc454e27f800365d5c9f8c0194834e
husheric <ericliu@ac.c4q.nyc>
2018-03-22 14:28:40 -0400

    added very few styles

--- frontend/src/Components/Homepage/Components/HomepageComponent.js
+++ frontend/src/Components/Homepage/Components/HomepageComponent.js
@@ -1,85 +1,68 @@
 import React, { Component } from "react"
+import '../Styles/Homepage.css';
+
 
 class HomepageComponent extends Component {
   render() {
     const { handleChange, handleLogin, handleSignUp } = this.props;
     return (
-      <div>
-
-        <div className='login_menu'>
-          <div className='logo'>
-            <img src='https://i.imgur.com/m6bz64S.png' />
-            <h1>RoomEase</h1>
-          </div>
-          <div className='login_form'>
-            <form onSubmit={handleLogin}>
-              <input type='text' placeholder='username' name='loginUsername' onChange={handleChange} />
-              <input type='password' placeholder='password' name='loginPassword' onChange={handleChange} />
-              <input type='submit' value='login'/>
-            </form>
-          </div>
+      <div className='homepage_container'>
+        
+        <div className='logo'>
+          <img src='https://i.imgur.com/m6bz64S.png' />
+          <h2>RoomEase</h2>
+        </div>
+        <div className='login_form'>
+          <form onSubmit={handleLogin}>
+            <input type='text' placeholder='username' name='loginUsername' onChange={handleChange} />
+            <input type='password' placeholder='password' name='loginPassword' onChange={handleChange} />
+            <input type='submit' value='login'/>
+          </form>
         </div>
 
+        <div className='about_us'>
+          <h1>About Us</h1>
+          <p>blah blah blah</p>
+        </div>
 
-
-        <div className='signup_body'>
-          <div className='about_us'>
-            <h1>About Us</h1>
-            <p>blah blah blah</p>
-          </div>
-          <div className='signup_form'>
-            <form onSubmit={handleSignUp}>
-          <label>
-            Name:
+        <div className='signup_form'>
+          <form onSubmit={handleSignUp}>
             <input
               type="text"
               name="name"
+              placeholder='Full Name'
               onChange={handleChange}
             />
-          </label> {" "}
-          <label>
-            Username:
             <input
               type="text"
               name="username"
+              placeholder='Username'
               onChange={handleChange}
             />
-          </label> {" "}
-          <label>
-            Email:
             <input
-              type="text"
+              type="email"
               name="email"
+              placeholder='Email'
               onChange={handleChange}
             />
-          </label>
-          {" "}
-          <label>
-            Password:
             <input
               type="password"
               name="password"
+              placeholder='Password'
               onChange={handleChange}
             />
-          </label>  {" "}
-          <label>
-            Phone Number:
             <input
               type="tel"
               name="phone"
+              placeholder='Phone Number'
               onChange={handleChange}
             />
-          </label>  {" "}
-          <label>
             <input name="gender" type="radio" value="M" /> Male
-          </label> {" "}
-          <label>
             <input name="gender" type="radio" value="F" /> Female
-          </label>
-          <button>Submit</button>
-        </form>
-          </div>
+            <input className='signup_submit' type='submit' value='Sign Up' />
+          </form>
         </div>
+
       </div>
     )
   }
