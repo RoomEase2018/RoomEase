959f0d185506c433ef42fe726fd09af1d306f510
Aiden <aidenshih@gmail.com>
2018-03-22 17:28:45 -0400

    Move display component into components folder and container into container. Added SASS css to the login page.

--- frontend/src/Components/Homepage/Containers/HomepageContainer.jsx
+++ frontend/src/Components/Homepage/Containers/HomepageContainer.jsx
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
 import Homepage from '../Components/Homepage';
+import "../Styles/Homepage.css"
 
 class HomepageContainer extends Component {
-    render() {
+    constructor() {
+        super();
+    
+        this.state = {
+    
+          // for login
+          loginUsername: '',
+          loginPassword: '',
+    
+          // for signup
+          name: "",
+          username: "",
+          email: "",
+          password: "",
+          gender: "",
+          phone: ''
+    
+        };
+      }
+    
+      handleChange = e => {
+        this.setState({
+          [e.target.name]: e.target.value
+        });
+      };
+    
+      handleLogin = e => {
+        e.preventDefault();
+      }
+    
+      handleSignUp = e => {
+        e.preventDefault();
+      }
+    
+      render() {
+        const { handleChange, handleLogin, handleSignUp } = this;
+        console.log(this.state)
         return (
-            <div>
-                <Homepage />
-            </div>
-        );
-    }
+          <div className="RoomEase">
+            <Homepage handleChange={handleChange} handleLogin={handleLogin} handleSignUp={handleSignUp} />
+          </div>
+        )
+      }
 }
 
 export default HomepageContainer;
--- frontend/src/Components/RoomEaseContainer.jsx
+++ frontend/src/Components/RoomEaseContainer.jsx
@@ -2,14 +2,14 @@ import React, { Component } from "react"
 import { connect } from "react-redux" // ALLOWS THE USE OF REDUC TO GET THE STATE FROM THIS.PROPS
 import { Route, Switch } from "react-router-dom";
 
-import Homepage from "./Homepage/Homepage";
+import HomepageContainer from "./Homepage/Containers/HomepageContainer";
 import Setup from "./Setup/Setup";
 
 class RoomEaseContainer extends Component {
   render() {
     return(
       <Switch>
-        <Route exact path="/" component={Homepage} />
+        <Route exact path="/" component={HomepageContainer} />
         <Route path="/setup" component={Setup} />
       </Switch>
     )
