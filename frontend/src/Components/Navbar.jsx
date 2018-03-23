2175c44600ff53fbae718e5a6399ee7804ce55b4
Aiden <aidenshih@gmail.com>
2018-03-23 04:38:34 -0400

    added icons for navbar

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React from "react"
 
-const Navbar = (props) => (
+const Navbar = () => (
     <div>
-        Navbar
-        {props.children}
+        <div className="logo">
+            <img src="https://i.imgur.com/m6bz64S.png" height="70px" />
+            <h1>RoomEase</h1>
+        </div>
+        <div className="buttons">
+            <i class="fas fa-tv" />
+            <i class="far fa-user" />
+            <i class="fas fa-home" />
+            <i class="fas fa-plus" />
+            <i class="fas fa-cog" />
+        </div>
     </div>
-);
+)
 
-export default Navbar;
+export default Navbar
