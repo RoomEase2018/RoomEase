820ec56bedc60f44a08d1288e5ab7ca01cf94e94
Aiden <aidenshih@gmail.com>
2018-03-23 12:47:59 -0400

    Commit so ryan can pull

--- frontend/src/Components/Navbar/Components/Navbar.jsx
+++ frontend/src/Components/Navbar/Components/Navbar.jsx
@@ -1,24 +1,31 @@
 import React from "react"
-import '../Styles/Navbar.css'
+import { Link } from "react-router-dom"
+import "../Styles/Navbar.css"
 
 const Navbar = () => (
-        <div className="navbar">
-            <div className="navbar_logo">
-                <img
-                    className="navbar_logo_icon"
-                    src="https://i.imgur.com/m6bz64S.png"
-                    height="70px"
-                />
-                <h1 className="navbar_logo_name">RoomEase</h1>
-            </div>
-            <div className="navbar_buttons">
-                <i class="fas fa-tv" />
-                <i class="fas fa-user" />
-                <i class="fas fa-home" />
-                <i class="fas fa-plus" />
-                <i class="fas fa-cog" />
-            </div>
+    <div className="navbar">
+        <div className="navbar_logo">
+            <img
+                className="navbar_logo_icon"
+                src="https://i.imgur.com/m6bz64S.png"
+                height="70px"
+            />
+            <h1 className="navbar_logo_name">RoomEase</h1>
         </div>
+        <div className="navbar_buttons">
+            <Link to="/dashboard">
+                <i class="fas fa-tv" />
+            </Link>{" "}
+            <Link to="/">
+                <i class="fas fa-user" />
+            </Link>{" "}
+            <Link to="">
+                <i class="fas fa-home" />
+            </Link>{" "}
+            <i class="fas fa-plus" />{" "}
+            <i class="fas fa-cog" />
+        </div>
+    </div>
 )
 
 export default Navbar
