15ab967a5ad58125dc2a0c32f34af4901e006298
ryanomor <omoruyi.ryan@gmail.com>
2018-03-27 23:33:19 -0400

    fix npm error

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,21 +1,21 @@
-// var express = require('express');
-// var router = express.Router();
-// let db = require("../db/queries");
-// const passport = require("../auth/local");
+var express = require('express');
+var router = express.Router();
+let db = require("../db/queries");
+const passport = require("../auth/local");
 
-// /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
 
-// router.post("/new", db.createUser);
+router.post("/new", db.createUser);
 
-// router.post("/login", passport.authenticate("local"), (req, res) => {
-//   res.json(req.user);
-// });
+router.post("/login", passport.authenticate("local"), (req, res) => {
+  res.json(req.user);
+});
 
-// router.get("/logout", db.logoutUser);
+router.get("/logout", db.logoutUser);
 
-// router.get("/user/:id", db.getUserById);
+router.get("/user/:id", db.getUserById);
 
-// module.exports = router;
+module.exports = router;
