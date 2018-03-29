5720eb50fd87f2366da93b6cd5989d89edc50d01
Ruben Profit <rubenprofit@ac.c4q.nyc>
2018-03-29 11:14:11 -0400

    state changes

--- frontend/src/Components/Dashboard/Reducers/DashboardReducer.js
+++ frontend/src/Components/Dashboard/Reducers/DashboardReducer.js
@@ -1,12 +1,3 @@
-import {
-  RECEIVE_ALL_ACTIVE_TASK,
-  RECEIVE_ALL_ACTIVE_RECURRING_TASKS,
-  RECEIVE_ALL_ACTIVE_EXPENSES,
-  RECEIVE_ALL_ACTIVE_RECURRING_EXPENSES,
-  RECEIVE_ALL_VISIBLE_NOTES,
-  RECEIVE_ALL_ACTIVE_GOALS
-} from "../Actions/DashboardActions";
-
 const defaultState = {
   allActiveTasks: {
     tasks: [],
@@ -24,22 +15,33 @@ export default (state = defaultState, action) => {
   let newState = state;
 
   switch (action.type) {
-    case "RECEIVE_ALL_ACTIVE_TASK":
-      return newState.allActiveTasks.tasks = action.tasks
+    case "GET_ALL_ACTIVE_TASK":
+      return {
+        allActiveTasks: {
+          tasks: [ ...newState.allActiveTasks.tasks, {}],
+          recurringTasks: newState.allActiveTasks.recurringTasks
+        },
+        allActiveExpenses: {
+          expenses: newState.allActiveExpenses.expenses,
+          recurringExpenses: newState.allActiveExpenses.recurringExpenses
+        },
+        notes: newState.notes,
+        goals: newState.goals
+      }
 
-    case "RECIEVE_ALL_ACTIVE_RECURRING_TASK":
+    case "GET_ALL_ACTIVE_RECURRING_TASK":
       return newState.allActiveTasks.recurringTasks = action.tasks
 
-    case "RECEIVE_ALL_ACTIVE_EXPENSES":
+    case "GET_ALL_ACTIVE_EXPENSES":
       return newState.allActiveExpenses.expenses = action.expenses
 
-    case "RECEIVE_ALL_ACTIVE_RECURRING_EXPENSES":
+    case "GET_ALL_ACTIVE_RECURRING_EXPENSES":
       return newState.allActiveExpenses.recurringExpenses = action.expenses
 
-    case "RECEIVE_ALL_VISIBLE_NOTES":
+    case "GET_ALL_VISIBLE_NOTES":
       return newState.notes = action.notes
 
-    case "RECEIVE_ALL_ACTIVE_GOALS":
+    case "GET_ALL_ACTIVE_GOALS":
       return newState.goals = action.goals
 
     default:
