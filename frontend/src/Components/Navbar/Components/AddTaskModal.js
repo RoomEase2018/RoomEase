714d6841cd854f47d5ab0c0415c3f92082136574
ryanomor <omoruyi.ryan@gmail.com>
2018-04-02 16:56:50 -0400

    still working on navbar layout

---
+++ frontend/src/Components/Navbar/Components/AddTaskModal.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../Styles/ModalStyles.css";
+
+class AddTaskModal extends React.Component {
+    render() {
+        return (
+            <div className="modal">
+                
+            </div>
+        );
+    }
+}
+
+export default AddTaskModal;
