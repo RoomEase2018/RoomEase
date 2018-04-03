092e88418d003798b3955645c09375e8abf20a96
ruben <rubenprofit@ac.c4q.nyc>
2018-04-03 00:52:26 -0400

    States (#60)
    * dashboard actions/reducers
    
    * dashboard action/reducer
    
    * dashboard actions
    
    * sassStyle error
    
    * stashing
    
    * deleted extra scss file which caused error
    
    * removed scss which caused error rendering
    
    * state changes
    
    * for redux
    
    * stashing actions and states
    
    * dashboard actions and state finito
    
    * commit for presentation
    
    * dashboard wired up just needs to render proper info
    
    * apartment state init
    
    * apartment profile state init

--- frontend/src/Components/Apartment/Components/ApartmentComponent.jsx
+++ frontend/src/Components/Apartment/Components/ApartmentComponent.jsx
@@ -1,26 +1,32 @@
-import React from "react"
-import AptProgress from "./AptProgress";
+import React from "react";
 
-const ApartmentComponent = () => (
-    <div className="apt">
-        <div className="apt_banner_container">
+const ApartmentComponent = ({ info }) => {
+    console.log(info)
+  return (
+    <div>
+      {info ? (
+        <div className="apt">
+          <div className="apt_banner_container">
             <h1 className="banner">
-                <span>Room Name</span>
+              <span>{info[0].apt_name}</span>
             </h1>
-        </div>
-        <div className='apt_rankings_container' >
+          </div>
+          <div className="apt_rankings_container">
             <h2>Apartment Rankings</h2>
             <div>Name List</div>
-            <div>Karma List</div>
-        </div>
-        <div className='apt_rewards_container' >
+            <div>Karma: {info[0].karma}</div>
+          </div>
+          <div className="apt_rewards_container">
             <h2>Rewards</h2>
             <div>Goal List</div>
             <div>Requirements List</div>
-            <AptProgress />
+          </div>
         </div>
-
+      ) : (
+        <div> loading ... </div>
+      )}
     </div>
-)
+  );
+};
 
-export default ApartmentComponent
+export default ApartmentComponent;
